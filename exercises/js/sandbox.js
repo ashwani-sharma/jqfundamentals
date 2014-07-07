9247298ee604d793b0ca4d26af49b2dda49c3dab
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-07-07 17:17:58 +0530

    exercise #2.2 added

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -1,3 +1,5 @@
+// =========== Exercise 2.1 Selecting ============
+
 // #1 selecting all div elements having class 'module'
 $('div.module');
 
@@ -17,3 +19,25 @@ $('img[alt]').length;
 
 // #6 selecting all of the odd table rows in the table body
 $('table tr:odd');
+
+
+
+
+// =========== Exercise 2.2 Traversing ============
+
+// #1 selecting all image elements and logging their alt attributes
+$('img').each(function () {
+	console.log($(this).attr('alt'));
+});
+
+// #2 selecting the search input text box, then traverse up to the form and add a class to the form
+$('input[name="q"]').parent('form').addClass('traversed');
+
+// #3 selecting the list item inside #myList that has a class of "current" and remove that class from it; add a class of "current" to the next list item
+$('#myList li.current').removeClass('current').next('li').addClass('current');
+
+// #4 selecting the select element inside #specials; traverse your way to the submit button
+$('#specials select').parent('li').next('li').children('input[type="submit"]');
+
+// #5 selecting the first list item in the #slideshow element; add the class "current" to it, and then add a class of "disabled" to its sibling elements.
+$('#slideshow').children('li').first().addClass('current').siblings().addClass('disabled');
