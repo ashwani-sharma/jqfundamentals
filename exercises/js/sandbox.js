ae937bd269e3b579de5b40c62b5c3981435759db
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-07-07 19:16:34 +0530

    exercise #2.3 added

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -41,3 +41,29 @@ $('#specials select').parent('li').next('li').children('input[type="submit"]');
 
 // #5 selecting the first list item in the #slideshow element; add the class "current" to it, and then add a class of "disabled" to its sibling elements.
 $('#slideshow').children('li').first().addClass('current').siblings().addClass('disabled');
+
+
+
+
+
+// =========== Exercise 2.3 Manipulating ============
+
+$(document).ready (function () {
+	// #1 adding five new list items to the end of the unordered list #myList
+  for(i = 0; i < 5; i++) {
+  	$('<li>List Item ' + parseInt($('#myList li').length + 1) + '</li>').insertAfter($('#myList li').last());
+  }
+
+  // #2 removing the odd list items
+  $('#myList li:odd').remove();
+
+  // #3 adding another h2 and another paragraph to the last div.module
+  $('div.module').last().append('<h2>Another Added Heading</h2><p>Another added paragraph</p>');
+
+  // #4 adding another option to the select element; give the option the value "Wednesday"
+  $('div.module:last select option[value="tuesday"]').after('<option value="Wednesday">Wednesday</option>');
+
+  // #5 adding a new div.module to the page after the last one; putting a copy of one of the existing images inside of it.
+  $('div.module:last').after('<div class="module"></div>');
+  $('img:first').clone().appendTo('div.module:last');
+});
