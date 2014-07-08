cfed52c5ce953af9c705a89e8d34ed6828906da1
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-07-08 15:21:21 +0530

    exercise #4.1 indentation fixes

--- exercises/js/inputHint.js
+++ exercises/js/inputHint.js
@@ -1,52 +1,52 @@
 var InputHint = function () {
-	this.label = $('label[for="q"]');
-	this.labelValue = this.label.text();
-	this.searchField = $('input.input_text');
+  this.label = $('label[for="q"]');
+  this.labelValue = this.label.text();
+  this.searchField = $('input.input_text');
 }
 
 InputHint.prototype.changeLabels = function () {
-	var _this = this;
+  var _this = this;
 
-	_this.searchField.val(_this.labelValue).addClass('hint');
-	_this.label.detach();
+  _this.searchField.val(_this.labelValue).addClass('hint');
+  _this.label.detach();
 }
 
 InputHint.prototype.bindEvents = function () {
-	this.bindFocus();
-	this.bindBlur();
+  this.bindFocus();
+  this.bindBlur();
 }
 
 InputHint.prototype.bindFocus = function () {
-	var _this = this;
+  var _this = this;
 
-	_this.searchField.on('focus', function () {
-		var currentInputVal = _this.searchField.val();
+  _this.searchField.on('focus', function () {
+    var currentInputVal = _this.searchField.val();
 
-		if(currentInputVal == _this.labelValue) {
-			_this.searchField.val('');
-			_this.searchField.removeClass('hint');
-		}
-		else {
-			_this.searchField.val(currentInputVal);
-		}
-	});
+    if(currentInputVal == _this.labelValue) {
+      _this.searchField.val('');
+      _this.searchField.removeClass('hint');
+    }
+    else {
+      _this.searchField.val(currentInputVal);
+    }
+  });
 }
 
 InputHint.prototype.bindBlur = function () {
-	var _this = this;
+  var _this = this;
 
-	_this.searchField.on('blur', function () {
-		var currentInputVal = _this.searchField.val();
+  _this.searchField.on('blur', function () {
+    var currentInputVal = _this.searchField.val();
 
-		if(currentInputVal.trim() == '') {
-			_this.searchField.val(_this.labelValue);
-			_this.searchField.addClass('hint');
-		}
-	});
+    if(currentInputVal.trim() == '') {
+      _this.searchField.val(_this.labelValue);
+      _this.searchField.addClass('hint');
+    }
+  });
 }
 
 $(document).ready(function () {
-	var checkResult = new InputHint();
-	checkResult.changeLabels();
-	checkResult.bindEvents();
+  var checkResult = new InputHint();
+  checkResult.changeLabels();
+  checkResult.bindEvents();
 });
