92e2a8c88304f44d56c255ec6c5c5db92079ad80
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-07-09 15:22:47 +0530

    exercise #5.2 navigation drop-downs added

---
+++ exercises/js/navigation.js
@@ -0,0 +1,16 @@
+var DropDownMenu = function ($li) {
+  this.$li = $li;
+}
+
+DropDownMenu.prototype.bindEvent = function () {
+  $li.hover(function () {
+  	$(this).addClass('hover').children('ul').slideDown();
+  }, function () {
+    $(this).removeClass('hover').children('ul').slideUp();
+  });
+}
+
+$(document).ready(function () {
+	$li = $('#nav > li');
+  (new DropDownMenu($li)).bindEvent();
+});
