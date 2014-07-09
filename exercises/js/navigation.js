e0ceeeb76941ac59a209bbbf4747775e960d2330
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-07-09 15:24:04 +0530

    exercise #5.2 indentation fixes

--- exercises/js/navigation.js
+++ exercises/js/navigation.js
@@ -4,13 +4,13 @@ var DropDownMenu = function ($li) {
 
 DropDownMenu.prototype.bindEvent = function () {
   $li.hover(function () {
-  	$(this).addClass('hover').children('ul').slideDown();
+    $(this).addClass('hover').children('ul').slideDown();
   }, function () {
     $(this).removeClass('hover').children('ul').slideUp();
   });
 }
 
 $(document).ready(function () {
-	$li = $('#nav > li');
+  $li = $('#nav > li');
   (new DropDownMenu($li)).bindEvent();
 });
