8434b76247a907d563dece4aa88f72929986b2e9
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-09-11 16:09:14 +0530

    indentation fixes

--- exercises/product_data/js/product-grid.js
+++ exercises/product_data/js/product-grid.js
@@ -1,90 +1,90 @@
 var ProductGrid = function ($productContainer, $filterHolder, $checkBox) {
-	this.$productContainer = $productContainer;
-	this.$filterHolder = $filterHolder;
-	this.$checkBox = $checkBox;
+  this.$productContainer = $productContainer;
+  this.$filterHolder = $filterHolder;
+  this.$checkBox = $checkBox;
 }
 
 ProductGrid.prototype.init = function () {
-	this.loadJson();
-	this.bindEvent();
+  this.loadJson();
+  this.bindEvent();
 }
 
 ProductGrid.prototype.loadJson = function () {
-	var _this = this;
+  var _this = this;
 
-	$.ajax ({
-		url: 'data/product.json',
-		method: 'get',
-		dataType: 'json'
-	}).success(function (data) {
-		_this.createHtmlData(data);
-	})
+  $.ajax ({
+    url: 'data/product.json',
+    method: 'get',
+    dataType: 'json'
+  }).success(function (data) {
+    _this.createHtmlData(data);
+  });
 }
 
 ProductGrid.prototype.createHtmlData = function (data) {
-	var _this = this;
-	for(var i = 0; i < data.length; i = i + 1) {
-		var url = data[i].url,
-				brand = data[i].brand,
-				color = data[i].color,
-				soldOut = data[i].sold_out,
-				img = '<img src="images/'  + url +'"/>'
-				$productBox = $('<span></span>').attr({'id': i, 'data-brand': brand, 'data-color': color, 'data-sold-out': soldOut}).addClass('box').appendTo(_this.$productContainer);
+  var _this = this;
+  for(var i = 0; i < data.length; i = i + 1) {
+    var url = data[i].url,
+        brand = data[i].brand,
+        color = data[i].color,
+        soldOut = data[i].sold_out,
+        img = '<img src="images/'  + url +'"/>'
+        $productBox = $('<span></span>').attr({'id': i, 'data-brand': brand, 'data-color': color, 'data-sold-out': soldOut}).addClass('box').appendTo(_this.$productContainer);
 		
-		$(img).appendTo($productBox);
+    $(img).appendTo($productBox);
 	}
 }
 
 ProductGrid.prototype.bindEvent = function () {
-	var _this = this;
+  var _this = this;
 
-	_this.$checkBox.on('change', function () {
-		_this.filteredData();
-	});
+  _this.$checkBox.on('change', function () {
+    _this.filteredData();
+  });
 }
 
 ProductGrid.prototype.showFilteredProducts = function (filters) {
-	$('span.box').hide();
-	$(filters).show();
+  $('span.box').hide();
+  $(filters).show();
 }
 
 ProductGrid.prototype.filteredData = function () {
-	var _this = this,
-			$holder = _this.filterHolder,
-			$product = $('span.box');
+  var _this = this,
+      $holder = _this.filterHolder,
+      $product = $('span.box');
 
-	_this.$filterHolder.each(function (i, $holder) {
-		$product = _this.dataStorage($product, $holder);
-	});
+  _this.$filterHolder.each(function (i, $holder) {
+    $product = _this.dataStorage($product, $holder);
+  });
 
-	_this.showFilteredProducts($product);
+  _this.showFilteredProducts($product);
 }
 
 ProductGrid.prototype.dataStorage = function (elem, holder) {
-	var checkedBox = $(holder).find('.checkbox:checked');
+  var checkedBox = $(holder).find('.checkbox:checked');
 
-	if(checkedBox.length != 0) {
-		var array = [];
+  if(checkedBox.length != 0) {
+    var array = [];
 
-		checkedBox.each(function () {
-			var $this = $(this),
-					filterName = $this.attr('name'),
-					valueSelected = $this.attr('data-' + filterName);
+    checkedBox.each(function () {
+      var $this = $(this),
+          filterName = $this.attr('name'),
+          valueSelected = $this.attr('data-' + filterName);
 
-			$(elem).each(function () {
-				if($(this).attr('data-' + filterName) == valueSelected) {
-					array.push(this);
-				}
-			});
-		});
-		return array;
-	}
-	else {
-		return elem;
-	}
+      $(elem).each(function () {
+        if($(this).attr('data-' + filterName) == valueSelected) {
+          array.push(this);
+        }
+      });
+    });
+    return array;
+  }
+  else {
+    return elem;
+  }
 }
 
 $(function () {
-	var productGrid = new ProductGrid($('#products'), $('ul.block'), $('ul.block .checkbox'));
-	productGrid.init();
+  var productGrid = new ProductGrid($('#products'), $('ul.block'), $('ul.block .checkbox'));
+  productGrid.init();
 });
