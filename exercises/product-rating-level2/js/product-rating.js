91e4c916916f054869f7c389017ca18504e69f7b
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-09-16 19:55:48 +0530

    product rating js updated with new methods

--- exercises/product-rating-level2/js/product-rating.js
+++ exercises/product-rating-level2/js/product-rating.js
@@ -34,7 +34,7 @@ ProductRating.prototype.createProductsWithRadioButtons = function () {
     $('<label/>').attr('for', dataProducts[i].item).attr('id', dataProducts[i].item).addClass('prod-item').html(dataProducts[i].item).appendTo(radioHolder);
     
     for (var j = 0; j < dataRatings.length; j++) {
-      var radio = $('<input/>').addClass(dataRatings[j].type.slice(0, dataRatings[j].type.indexOf(' ')).toLowerCase()).attr({'name': dataProducts[i].item, 'type': 'radio', 'disabled': true}),
+      var radio = $('<input/>').addClass(dataRatings[j].type.slice(0, dataRatings[j].type.indexOf(' ')).toLowerCase()).attr({'name': dataProducts[i].item, 'type': 'radio'}),
       $span = $('<span/>').addClass('radios');
 
       $span.append(radio).appendTo(radioHolder);
@@ -43,27 +43,41 @@ ProductRating.prototype.createProductsWithRadioButtons = function () {
 }
 
 ProductRating.prototype.bindEvents = function () {
-  this.$container.delegate('.prod-item', "click", this.actionOnClickingProductLabels);
-  this.$container.delegate('.ratings', "click", this.actionOnClickingRatingLables);
+  this.actionOnClickingRadioButtons();
+  this.actionOnClickingProductLabels();
+  this.actionOnClickingRatingLables();
 }
 
-ProductRating.prototype.actionOnClickingRatingLables = function () {
-  var $this = $(this);
-  $('.highlighted').each(function () {
-    $this.addClass('highlighted').siblings('.ratings').removeClass('highlighted');
-    $(this).siblings('span').find('.' + $this.attr('id')).prop({disabled: false, checked: true}).parent().siblings('span').children().prop({checked: false});
+ProductRating.prototype.actionOnClickingRadioButtons = function () {
+  this.$container.delegate('input[type="radio"]', 'click', function () {
+    var $this = $(this);
+    $('.rate-row').find('#' + $this.attr('class')).addClass('highlighted').siblings().removeClass('highlighted');
+    $('.radio-box').find('#' + $this.attr('name')).addClass('highlighted').parent().siblings().find('.prod-item').removeClass('highlighted ');
   });
 }
 
 ProductRating.prototype.actionOnClickingProductLabels = function () {
-  var $this = $(this);
-  $('.prod-item.selected').removeClass('selected');
-  $this.toggleClass('selected highlighted');
-  $('.prod-item.selected').parent('.radio-box').siblings().find('.prod-item').removeClass('highlighted');
-  if(!($('.prod-item').hasClass('highlighted'))) {
-    $('.ratings').removeClass('highlighted');
-  };
-  // $this.parent('.radio-box').find('input[name = "' + $this.text() + '"]').prop('disabled', false);
+  this.$container.delegate('.prod-item', 'click', function () {
+    var $this = $(this);
+    $('.prod-item.selected').removeClass('selected');
+    $this.toggleClass('selected highlighted');
+    $('.prod-item.selected').parent('.radio-box').siblings().find('.prod-item').removeClass('highlighted');
+    $('.rate-row').find('.highlighted').removeClass('highlighted');
+    $('.rate-row').find('#' + $this.parent().find('input:checked').attr('class')).addClass('highlighted');
+    if(!($('.prod-item').hasClass('highlighted'))) {
+      $('.ratings').removeClass('highlighted');
+    };
+  });
+}
+
+ProductRating.prototype.actionOnClickingRatingLables = function () {
+  this.$container.delegate('.ratings', 'click', function () {
+    var $this = $(this);
+    $('.highlighted').each(function () {
+      $this.addClass('highlighted').siblings('.ratings').removeClass('highlighted');
+      $(this).siblings('span').find('.' + $this.attr('id')).prop('checked', true).parent().siblings('span').children().prop('checked', false);
+    });
+  });
 }
 
 $(function () {
