fb1e7eca7e9ba05f68e8d431f24c86584a0f3670
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-09-19 13:16:23 +0530

    validation method added

--- exercises/contact-manager-level2/js/contact-manager.js
+++ exercises/contact-manager-level2/js/contact-manager.js
@@ -2,7 +2,7 @@ var ContactManager = function ($required, $email, $search) {
   this.$required = $required;
   this.$email = $email;
   this.$search = $search;
-  this.emailRegEx = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]{2,}(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+  this.emailRegEx = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
 }
 
 ContactManager.prototype.initMethods = function () {
@@ -17,14 +17,14 @@ ContactManager.prototype.createContactBlocks = function () {
   $('#add-contact').on('click', function () {
     var inputValues = [];
 
-    if(_this.getValuesFromInputs(inputValues, _this.$email, _this.emailRegEx)) {
+    if(_this.getValuesFromInputs(inputValues) && _this.validateEmailField(_this.$email, _this.emailRegEx)) {
       _this.$required.val('');
       _this.createContactBlock(inputValues[0], inputValues[1]);
     }
   });
 }
 
-ContactManager.prototype.getValuesFromInputs = function (valuesArray, input, pattern) {
+ContactManager.prototype.getValuesFromInputs = function (valuesArray) {
   var _this = this,
       len = _this.$required.length;
 
@@ -34,16 +34,20 @@ ContactManager.prototype.getValuesFromInputs = function (valuesArray, input, pat
       _this.$required[i].focus();
       return false;
     }
-    // else if(!pattern.test(input.value)) {
-    //   alert('Please enter valid email');
-    //   input.focus();
-    //   return false;
-    // }
     else {
       valuesArray.push(_this.$required[i].value);
     }
   }
   return valuesArray;
+}
+
+ContactManager.prototype.validateEmailField = function (input, pattern) {
+  if(!pattern.test(input.val())) {
+    alert('Please enter valid email');
+    input.focus();
+    return false;
+  }
+  return true;
 }
 
 ContactManager.prototype.createContactBlock = function (elem1, elem2) {
@@ -72,15 +76,15 @@ ContactManager.prototype.searchBlockByEnteredName = function () {
 
   _this.$search.keyup(function () {
     var text = $(this).val().toLowerCase().trim(),
-        $nameBlock = $('#container').find('.contact-block'),
-        len = $nameBlock.length;
+        $nameBlock = $('#container').find('.contact-block');
 
     $nameBlock.each(function () {
-      if($(this).data('name').indexOf(text) == 0) {
-        $(this).show();
+      var $this = $(this);
+      if($this.data('name').indexOf(text) == 0) {
+        $this.show();
       }
       else {
-        $(this).hide();
+        $this.hide();
       }
     });
   });
