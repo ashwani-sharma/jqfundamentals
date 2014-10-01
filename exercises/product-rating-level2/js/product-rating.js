8f2be0068e25f3ea0b700dfcf57990076b72f2b7
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-10-01 20:26:41 +0530

    product rating exercise js and css updated

--- exercises/product-rating-level2/js/product-rating.js
+++ exercises/product-rating-level2/js/product-rating.js
@@ -1,7 +1,7 @@
-var ProductRating = function ($container) {
-  this.products = [{'item': 'Coffee'}, {'item': 'Tea'}, {'item': 'Sodas'}];
-  this.ratings =[{'type':'Love it'}, {'type':'Like it'}, {'type':'No Views'}, {'type':'Dislike it'}, {'type':'Abhor it'}];
+var ProductRating = function ($container, products, ratings) {
   this.$container = $container;
+  this.products = products;
+  this.ratings = ratings;
 }
 
 ProductRating.prototype.initMethods = function () {
@@ -10,77 +10,82 @@ ProductRating.prototype.initMethods = function () {
 }
 
 ProductRating.prototype.createBlocks = function () {
-  this.createRatingBlock();
-  this.createProductsWithRadioButtons();
+  this.createRatingLabels();
+  this.createProductsRow();
 }
 
-ProductRating.prototype.createRatingBlock = function () {
+ProductRating.prototype.createRatingLabels = function () {
   var _this = this,
       dataRatings = _this.ratings,
-      rateRow = $('<div/>').addClass('rate-row').appendTo(_this.$container);
+      ratingsArray = [];
 
   for(i = 0; i < dataRatings.length; i++) {
-    $('<label/>').attr('for', dataRatings[i].type).attr('id', dataRatings[i].type.slice(0, dataRatings[i].type.indexOf(' ')).toLowerCase()).addClass('ratings').text(dataRatings[i].type).appendTo(rateRow);
+    ratingsArray.push($('<label/>').attr('for', dataRatings[i].type).attr('id', dataRatings[i].type.replace(' ', '-').toLowerCase()).addClass('ratings').text(dataRatings[i].type));
   }
+  $('<div/>').addClass('rate-row').append(ratingsArray).appendTo(_this.$container);
 }
 
-ProductRating.prototype.createProductsWithRadioButtons = function () {
+ProductRating.prototype.createProductsRow = function () {
   var _this = this,
+      elemArray = [],
       dataRatings = _this.ratings,
       dataProducts = _this.products;
 
   for (var i = 0; i < dataProducts.length; i++) {
-    var radioHolder = $('<div/>').addClass('radio-block').appendTo(_this.$container);
-    $('<label/>').attr('for', dataProducts[i].item).attr('id', dataProducts[i].item).addClass('prod-item').html(dataProducts[i].item).appendTo(radioHolder);
-    
+    var productRow = $('<div/>').addClass('product-row'),
+        labels = $('<label/>').attr('for', dataProducts[i].item).attr('id', dataProducts[i].item).addClass('prod-item').html(dataProducts[i].item).appendTo(productRow);
+
+    elemArray.push(productRow);
+
     for (var j = 0; j < dataRatings.length; j++) {
-      var radio = $('<input/>').addClass(dataRatings[j].type.slice(0, dataRatings[j].type.indexOf(' ')).toLowerCase()).attr({'name': dataProducts[i].item, 'type': 'radio'}),
+      var radio = $('<input/>').attr({'name': dataProducts[i].item, 'type': 'radio', 'data-name': dataRatings[j].type.replace(' ', '-').toLowerCase()}),
       $span = $('<span/>').addClass('radios');
 
-      $span.append(radio).appendTo(radioHolder);
+      $span.append(radio).appendTo(productRow);
     }
-  }  
+  }
+  _this.$container.append(elemArray);
 }
 
 ProductRating.prototype.bindEvents = function () {
-  this.actionOnClickingRadioButtons();
-  this.actionOnClickingProductLabels();
-  this.actionOnClickingRatingLables();
+  this.bindEventOnRadioButtons();
+  this.bindEventOnProductLabels();
+  this.bindEventOnRatingLabels();
 }
 
-ProductRating.prototype.actionOnClickingRadioButtons = function () {
+ProductRating.prototype.bindEventOnRadioButtons = function () {
   this.$container.delegate('input[type="radio"]', 'click', function () {
     var $this = $(this);
-    $('.rate-row').find('#' + $this.attr('class')).addClass('highlighted').siblings().removeClass('highlighted');
-    $('.radio-block').find('#' + $this.attr('name')).addClass('highlighted').parent().siblings().find('.prod-item').removeClass('highlighted ');
+    $('.rate-row').find('#' + $this.data('name')).addClass('highlighted').siblings().removeClass('highlighted');
+    $('.product-row').find('#' + $this.attr('name')).addClass('highlighted').parent().siblings().find('.prod-item').removeClass('highlighted');
   });
 }
 
-ProductRating.prototype.actionOnClickingProductLabels = function () {
+ProductRating.prototype.bindEventOnProductLabels = function () {
   this.$container.delegate('.prod-item', 'click', function () {
     var $this = $(this);
     $('.prod-item.selected').removeClass('selected');
     $this.addClass('selected highlighted').parent().siblings().removeClass('selected highlighted');
-    $('.prod-item.selected').parent('.radio-block').siblings().find('.prod-item').removeClass('highlighted');
+    $('.prod-item.selected').parent('.product-row').siblings().find('.prod-item').removeClass('highlighted');
     $('.rate-row').find('.highlighted').removeClass('highlighted');
-    $('.rate-row').find('#' + $this.parent().find('input:checked').attr('class')).addClass('highlighted');
+    $('.rate-row').find('#' + $this.parent().find('input:checked').attr('data-name')).addClass('highlighted');
     if(!($('.prod-item').hasClass('highlighted'))) {
       $('.ratings').removeClass('highlighted');
     };
   });
 }
 
-ProductRating.prototype.actionOnClickingRatingLables = function () {
+ProductRating.prototype.bindEventOnRatingLabels = function () {
   this.$container.delegate('.ratings', 'click', function () {
     var $this = $(this);
     $('.highlighted').each(function () {
       $this.addClass('highlighted').siblings('.ratings').removeClass('highlighted');
-      $(this).siblings('span').find('.' + $this.attr('id')).prop('checked', true).parent().siblings('span').children().prop('checked', false);
+      $(this).siblings('span').find('input[data-name = "' + $this.attr('id') + '"]').prop('checked', true).parent().siblings('span').children().prop('checked', false);
     });
   });
 }
 
 $(function () {
-  var productRating = new ProductRating($('#products'));
+  var productRating = new ProductRating($('#products'), [{'item': 'Coffee'}, {'item': 'Tea'}, {'item': 'Sodas'}], [{'type':'Love it'}, {'type':'Like it'}, {'type':'No Views'}, {'type':'Dislike it'}, {'type':'Abhor it'}]);
   productRating.initMethods();
 });
