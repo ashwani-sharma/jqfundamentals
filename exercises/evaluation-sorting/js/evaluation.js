cafccb7d13c86b6571ca86ea262455c8a0373d5a
Ashwani Sharma <sharma.ashwani007@gmail.com>
2014-10-16 19:16:43 +0530

    js updated

--- exercises/evaluation-sorting/js/evaluation.js
+++ exercises/evaluation-sorting/js/evaluation.js
@@ -5,72 +5,56 @@ var PrioritySort = function ($ul, $li) {
 }
 
 PrioritySort.prototype.initMethods = function () {
-  this.sortListItemsByPriorityOrder();
-  this.displayLiCount();
-  this.addSeeAllLink();
-  this.bindSeeAllLink();
-  this.bindSeeLessLink();
+  this.getInitiallySortedItems(this.$ul);
+  this.displayLiCountWithSeeAllLink();
+  this.bindEvents();
 }
 
-PrioritySort.prototype.sortListItemsByPriorityOrder = function () {
+PrioritySort.prototype.getInitiallySortedItems = function (block) {
   var _this = this;
-  _this.$ul.each(function () {
+  block.each(function () {
     var $this = $(this);
-    $this.find(_this.$li).sort(function (a, b) {
-      return +a.getAttribute('priority-order') - +b.getAttribute('priority-order');
+    $this.find(_this.$li).sort(function (x, y) {
+      return x.getAttribute('priority-order') - y.getAttribute('priority-order');
     }).appendTo($this);
   });
 }
 
-PrioritySort.prototype.displayLiCount = function () {
+PrioritySort.prototype.displayLiCountWithSeeAllLink = function () {
   var _this = this;
 
   _this.$ul.each(function () {
     var $this = $(this),
-        count = $this.attr('initial-items-count');
+        count = $this.attr('initial-items-count'),
+        link = $('<a/>').attr('href', 'javascript:').addClass('see-all').text('see all'),
+        linkHolder = $('<li></li>').append(link);
 
-    $this.height(_this.$liHeight * count - 13);
+    linkHolder.insertAfter($this.find('li').eq(count - 1));
+    $this.height(_this.$liHeight * count);
   });
 }
 
-PrioritySort.prototype.addSeeAllLink = function () {
-  var _this = this;
-
-  _this.$ul.each(function () {
-    var $this = $(this),
-        count = $this.attr('initial-items-count');
-
-    $('<li><a href="javascript:" class="see-all">see all</a></li>').insertAfter($this.find('li').eq(count - 1));
-    $this.height($this.height() + _this.$liHeight);
-  });
+PrioritySort.prototype.bindEvents = function () {
+  this.bindSeeAllLink();
+  this.bindSeeLessLink();
 }
 
 PrioritySort.prototype.bindSeeAllLink = function () {
-  var _this = this,
-      arr = [];
+  var _this = this;
 
   _this.$ul.on('click', 'a.see-all', function () {
-    var $this = $(this);
+    var $this = $(this),
+        link = $('<a/>').attr('href', 'javascript:').addClass('see-less').text('see less'),
+        linkHolder = $('<li></li>').append(link);
+
     $this.closest(_this.$ul).css('height', 'auto');
 
     $this.closest(_this.$ul).html($this.closest(_this.$ul).find('li').sort(function(x, y) {
       return $(x).text() < $(y).text() ? -1 : 1;
     }));
 
-    // $this.closest(_this.$ul).find(_this.$li).each(function () {
-    //   arr.push($(this).text());
-    // });
-
-    // $this.closest(_this.$ul).find(_this.$li).remove();
-    // arr.sort();
-
-    // for(i = 0; i < arr.length; i++) {
-    //   $('<li class="row">' + arr[i] + '</li>').appendTo($this.closest(_this.$ul));
-    // }
-
-    $('<li><a href="javascript:" class="see-less">see less</a></li>').appendTo($this.closest(_this.$ul));
-    $this.closest('li').remove();
-    arr = [];
+    linkHolder.appendTo($this.closest(_this.$ul));
+    $this.closest('li').hide();
   });
 }
 
@@ -78,22 +62,20 @@ PrioritySort.prototype.bindSeeLessLink = function () {
   var _this = this;
   _this.$ul.on('click', 'a.see-less', function () {
     var $this = $(this),
-        count = $this.closest(_this.$ul).attr('initial-items-count');
+        count = $this.closest(_this.$ul).attr('initial-items-count'),
+        link = $('<a/>').attr('href', 'javascript:').addClass('see-all').text('see all'),
+        linkHolder = $('<li></li>').append(link);
     
-    $this.closest(_this.$ul).each(function () {
-      $(this).find('li').sort(function (a, b) {
-        return +a.getAttribute('priority-order') - +b.getAttribute('priority-order');
-      }).appendTo($(this));
-    });
+    _this.getInitiallySortedItems($this.closest(_this.$ul));
 
-    $this.closest(_this.$ul).height(_this.$liHeight * count + 8);
-    $('<li><a href="javascript:" class="see-all">see all</a></li>').insertAfter($this.closest(_this.$ul).find('li.row').eq(count - 1));
+    $this.closest(_this.$ul).height(_this.$liHeight * count);
+    linkHolder.insertAfter($this.closest(_this.$ul).find('li.row').eq(count - 1));
 
     $this.remove();
   });
 }
 
 $(function () {
-  var priority = new PrioritySort($('ul.priority-sort'), $('ul.priority-sort .row'));
+  var priority = new PrioritySort($('ul.priority-sort'), $('.row'));
   priority.initMethods();
 });
